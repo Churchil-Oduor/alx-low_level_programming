ff1808e2a93fb0182e49ab9ea3e8ae438d8e70df
Churchil-Oduor <churchilokechoduor@gmail.com>
2022-09-08 15:25:17 -0700

    Adding file  6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("Size of a char: "+sizeof(char)+" byte(s)\n");
+	printf("Size of a int: "+sizeof(int)+" byte(s)\n");
+	printf("Size of a long: "+sizeof(long)+" byte(s)\n");
+	printf("Size of a long long int : "+sizeof(unsigned long)+" byte(s)\n");
+	printf("Size of a float: "+sizeof(float)+" byte(s)\n");
+
+	return (0);
+}
