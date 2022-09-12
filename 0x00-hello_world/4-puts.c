01cb200b87c5e019bc0c09c6aa526b4095723c1e
Churchil-Oduor <churchilokechoduor@gmail.com>
2022-09-12 14:35:44 -0700

    Adding the corrected file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main - program starts here
- * Return - this program prints a string
+ * Return: this program prints a string
  */
-
 int main(void)
-{	
-	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
