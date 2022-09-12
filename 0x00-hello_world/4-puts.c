954ce892eb3bef1116b9de0e9945ed25b70180a1
Churchil-Oduor <churchilokechoduor@gmail.com>
2022-09-12 13:12:59 -0700

    Final corrections on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
- #include <stdio.h>
-#include <string.h>
+#include <stdio.h>
+/**
+ * main - program starts here
+ * Return - this program prints a string
+ */
 
-
-int main()
+int main(void)
 {	
 	
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
