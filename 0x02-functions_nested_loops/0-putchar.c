dfd7ac66f6a579f6cc81758663cfc2ee89e2d527
Churchil-Oduor <churchilokechoduor@gmail.com>
2022-09-13 23:25:55 -0700

    Added the function prototype main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Return: Returns 0 if it executes correctly
  */
 
 int main(void)
+{
+	print();
+	return (0);
+}
+/**
+ * print - prints the required text
+ * Return: returns void
+ */
+void print(void)
 {
 	printf("_putchar\n");
 }
