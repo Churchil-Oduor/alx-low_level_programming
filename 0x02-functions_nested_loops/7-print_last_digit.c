978b908408934a6486dfa71b62497f5a9dedeced
Churchil-Oduor <churchilokechoduor@gmail.com>
2022-09-14 12:21:55 -0700

    Adding the file 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - fx that prints the last digit
+ * @n: digit whose last digit is sought
+ * Return: Returns the last digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
