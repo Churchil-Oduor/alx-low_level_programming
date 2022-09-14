e02cf0624a7e2f635b98824efc1703a794b090d6
Churchil-Oduor <churchilokechoduor@gmail.com>
2022-09-14 07:10:31 -0700

    adding 6_abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute
+ * Return: Returns the absolute integer value
+ */
+
+int _abs(int)
+{
+	if (int > 0)
+	{
+		return (int);
+	}
+	else
+	{
+		return (-1 * int);
+	}
+}
