2b1f4f952cc0a7ad783c5bdc22aa493a7d444e8e
Churchil-Oduor <churchilokechoduor@gmail.com>
2022-09-14 06:18:31 -0700

    Adding the file 5-sign and its prototype in main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * Return: Returns the sign of a number
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+, ");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0, ");
+		return (0);
+	}
+	else
+	{
+		printf("-, ");
+		return (-1);
+	}
+}
