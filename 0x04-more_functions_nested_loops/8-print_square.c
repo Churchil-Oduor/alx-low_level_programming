9d0537bbb8e6c5f20b71564973db2171bc347ff1
Churchil-Oduor <churchilokechoduor@gmail.com>
2022-09-16 08:52:18 -0700

    adding the file 8-print_square and prototype in main.h

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - fx that prints square
+ * @size: passed parm representing the size of square
+ * Return: void fx
+ */
+
+void print_square(int size)
+{
+	int l, w;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		for (w = 0 ; w < size ; w++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
