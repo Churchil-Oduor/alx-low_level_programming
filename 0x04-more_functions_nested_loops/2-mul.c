ce1b661e5216ae7e5985f685c1685bb98c538142
Churchil-Oduor <churchilokechoduor@gmail.com>
2022-09-16 12:16:21 +0300

    adding the file 2-mul.c and its prototype in main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - fx that multiplies two params passed to it
+ * @a: first param
+ * @b: second param
+ * Return: Returns the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
