1f1c3bba7759034db9aaafa3fd0b90e72ca6177e
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-03-27 23:15:59 +0300

    Correction on file 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - pointer to an int as parameter and updates the value it points to to 98
- *
+ * reset_to_98 - pointer to an int as parameter
+ * and updates the value it points to to 98
+ *@n: address passed here.
  * Return: void function.
  */
 
