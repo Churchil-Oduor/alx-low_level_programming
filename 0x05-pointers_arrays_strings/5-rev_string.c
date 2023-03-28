54ba1178ebe11cbfcb84f693c857dc9473809c82
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-03-28 17:37:01 +0300

    Correcting file 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,35 +1,40 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string.
- * @s: string entered to be reversed.
- * Return: void function.
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * rev_string - reverse a string
+ *@s: string to reverse
  */
 
 void rev_string(char *s)
 {
-	int c_chars, count;
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char tmp;
 
-	c_chars = 0;
-
-	while (*(s + c_chars) != '\0')
+	while (i < j)
 	{
-		++c_chars;
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++, j--;
 	}
-	printf("count: %d\n", c_chars);
-
-	char hold[c_chars];
-	count = c_chars;
-
-	while (--count != 0)
-		hold[count] = *(s + count);
-
-
-	count = -1;
-
-	while (++count != c_chars)
-		*(s + count) = hold[count];
-
-
-
 }
