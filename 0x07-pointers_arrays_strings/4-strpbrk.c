eeda3a4e3ea241ff3263b1c90ea8471796238817
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-04-03 17:35:33 +0300

    Working on corrections for file 4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strpbrk - locates the first occurence in the string s of any 
+ * _strpbrk - locates the first occurence in the string s of any
  * of the bytes in the string accept.
  *
  * @s: string to be scanned.
  * @accept: string to be scanned with.
- * Return: Returns a pointer to the byte in s that matches one of the bytes 
+ * Return: Returns a pointer to the byte in s that matches one of the bytes
  * in accept or NULL if no such byte is found.
  */
 
