c94b7f508269b4ba5746b629b3c2db146af9640f
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-04-12 23:50:52 +0300

    Cleaning work area

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into two words.
+ * @str: string to be split.
+ * Return: pointer to array of strings.
+ */
+
+char **strtow(char *str)
+{
+
+}
