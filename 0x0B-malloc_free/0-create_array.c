bbbc4db11be73b6fd74b945c853a2723d87fbc1e
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-04-12 16:23:52 +0300

    Syntax error correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,8 +21,8 @@ char *create_array(unsigned int size, char c)
 		{
 			for (index = 0; index < size; index++)
 				array[index] = c;
-			return (array);
+		return (array);
 		}
-	return (NULL);
 	}
+	return (NULL);
 }
