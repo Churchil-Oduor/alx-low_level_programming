3ce017fcad7a261aadcdd1e32b9092d8b8f1b288
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-04-13 23:31:37 +0300

    Correction

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	mem = malloc(sizeof(*mem) * (max - min + 1));
+	mem = malloc(sizeof(int) * (max - min + 1));
 
 	if (mem == NULL)
 		return (NULL);
