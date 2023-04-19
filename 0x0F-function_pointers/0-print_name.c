6597f7e7eaba8987e58ade4f366fdbf163d60fdb
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-04-20 01:21:10 +0300

    Adding files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print-name - prints a name.
+ *
+ * @name: name to be printed.
+ * @f: Callback function.
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - selects the correct function
+ * to perform the operation asked by the user.
+ *
+ * @a: 1st arg.
+ * @b: 2nd arg.
+ * @s: operator.
+ *
+ * Return: pointer to the correct function selected by user.
+ */
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"-", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+
+	int i;
+
+	i = 0;
+	if (s[0] == '+' || s[0] == '-' || s[0] == '*' ||
+			s[0] == '/' || s[0] == '%')
+	{
+		while (s[0] != ops[i].op[0])
+			i++;
+		return (ops[i].f);
+	}
+	return (NULL);
+}
