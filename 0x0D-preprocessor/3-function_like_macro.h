f5fbc0b042febdc128eaf0625c4bdcc4c646e293
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-06-20 07:37:10 +0300

    Adding Files

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FILE_H
+#define FILE_H
+
+#define ABS(x) ((x) > 0 ? (x) : -(x))
+#endif /* #ifndef FILE_H */
+
---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initiatiles a struct.
+ *
+ * @d: pointer to struct passed for initialization.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ *
+ * Return: Nothing.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
