b5fa82961299944c5e84c3c5c8d4399576a407d6
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-07-06 23:49:55 +0300

    adding files

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,7 @@ int _putchar(char c);
 unsigned int _strlen(const char *str);
 unsigned int _pow(unsigned int x, unsigned int y);
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
 
 
 #endif
