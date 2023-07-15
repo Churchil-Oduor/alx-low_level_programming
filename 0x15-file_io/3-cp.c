59342bb0f19e254a4fa9b8729769a73dc4f92295
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-07-15 05:57:36 +0300

    Corrections to match betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,11 +3,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #define BUFFSIZE 1024
-int _strlen(char *str);
+
 /**
  * main - copies content of  one file to another another file.
  * @argc: number of variables passed into the terminal.
  * @argv: array of string literals passed.
+ * Return: 0 always.
  */
 
 int main(int argc, char **argv)
@@ -32,8 +33,8 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT,
-		       	S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR |
+			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
@@ -54,63 +55,3 @@ int main(int argc, char **argv)
 	return (0);
 }
 
-/**
- * _strlen - gets length of string.
- *
- * @str: string to be scanned.
- * Return: length of string
- */
-
-int _strlen(char *str)
-{
-	int len;
-
-	len = 0;
-	while(str[len] != '\0')
-		len++;
-	return (len);
-}
-
-
-
-/**
- * str_concat - concatenates s2 to s1.
- * @s1: string to be concatenated with s2.
- * @s2: string to be concatenated with s1;
- * Return: pointer to memory with the concatenated strings.
- */
-
-char *str_concat(char *s1, char *s2)
-{
-	char *concat;
-	int num1, num2, index_1, index_2;
-
-	num1 = 0, num2 = 0, index_2 = 0;
-
-	if (*s1 || *s2 || s1 != NULL || s2 != NULL)
-	{
-		while (s1[num1] != '\0')
-			++num1;
-
-		while (s2[num2] != '\0')
-			++num2;
-
-		concat = malloc(sizeof(*concat) * (num1 + num2 + 1));
-
-		if (concat != NULL)
-		{
-			for (index_1 = 0; index_1 < (num1 + num2 + 1); index_1++)
-			{
-				if (index_1 < num1)
-					concat[index_1] = s1[index_1];
-				else if (index_1 < (num1 + num2))
-					concat[index_1] = s2[index_2++];
-				else
-					concat[index_1] = '\0';
-			}
-
-			return (concat);
-		}
-	}
-	return (NULL);
-}
