1b315bb73cb142528850289309fc37dab0cbbd44
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-09-15 18:44:01 +0300

    Corrections

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,7 +5,6 @@
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  *
- * @h: pointer holding the pointer to head node.
  * @idx: position of the node in the list starting from 0.
  * @n: data of the new node.
  * Return: node at position index else NULL if node does not exit.
@@ -28,14 +27,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (new == NULL)
 				return (NULL);
 
-			new->n = n;
-			new->prev = node->prev;
-			new->next = node;
+			if (node->next != NULL && node->prev != NULL)
+			{
+				new->n = n;
+				new->prev = node->prev;
+				new->next = node;
 
-			(node->prev)->next = new;
-			node->prev = new;
+				(node->prev)->next = new;
+				node->prev = new;
+				return (new);
+			}
+			else
+			{
+				new->n = n;
+				new->prev = NULL;
+				new->next = node;
 
-			return (new);
+				node->prev = new;
+				return (new);
+			}
 		}
 		i++;
 		node = node->next;
