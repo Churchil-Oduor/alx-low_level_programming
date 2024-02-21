427a775c8821ea88398c5ad350afd9a04ddd9b16
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-02-21 05:59:40 +0300

    Files

---
+++ sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - function that does sorting using
+ * bubble sort.
+ * @array: array to be sorted.
+ * @size: size of array to be sorted.
+ */
+
+void bubble_sort(int *array, size_t size)
+{}
